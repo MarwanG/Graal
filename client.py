a6d45b5825687625363a11bf764d6a0db08c11f6
stevenvar <steven.varoumas@gmail.com>
2014-12-28 17:27:49 +0100

    correction calcul rho, h et w
    Maintenant ça fonctionne bien

---
+++ client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2.7
+
+
+import sys
+import mmh3
+from hll import Hll
+
+class Client:
+	def __init__(self,p):
+		self.p = p
+
+
+	def readHash(self):
+		hll = Hll(self.p)
+		x = sys.stdin.readline().rstrip('\n')
+		while x:
+			hll.AddItem(mmh3.hash(x))
+			x = sys.stdin.readline().rstrip('\n')
+		print hll.Count()
+
+
+def main():
+   	client = Client(16)
+   	client.readHash()
+
+if __name__ == '__main__':
+  main()  
--- hll.py
+++ hll.py
@@ -8,53 +8,57 @@ import math
 class Hll:
     
     def __init__(self, p):
-       self.b = p;
+       self.b = 16;
        self.buckets = [];
-       self.alpha = 0.7;
        self.m = 2**self.b;
        for i in range (self.m):
            self.buckets.append(0);
 
-    def AddItem(self,h):
-  #      print 32;
-        j = h >> (32 -self.b);
-        w = h & (32 -self.b);
-        self.buckets[j] = max(  self.buckets[j], self.rho(w)  );
+    def alpha(self, m):
+        if m == 16:
+            return 0.673;
+        elif m == 32 :
+            return 0.697
+        elif m == 64:
+            return 0.709
+        return 0.7213/(1+ 1.079/m)
 
-    def count(self):
+    def AddItem(self,h):
+#        print bin(h)
+        h = h % (2**32)
+  #      print bin(h)
+        j = h & (self.m - 1)
+        w = h >> self.b
+#        print bin(h)
+#        print bin(j)
+ #       print bin(w)
+ #      str(self.rho(w))
+        self.buckets[j] = max(  self.buckets[j], self.rho(w)  );
+ 
+        #print "=";
+       # print str(self.buckets[j]);
+
+    def Count(self):
         sum = 0;
         for i in range(len(self.buckets)):
-            sum += 1/(2**(self.buckets[i]));
-            # print self.buckets[i]; JSS NOOB MAIS J'AIME PAS
+            sum += math.pow(2.0, -self.buckets[i])
+#            print self.buckets[i]; #JSS NOOB MAIS J'AIME PAS
         sum = 1.0/sum;
-        e = self.alpha*(len(self.buckets)**2)*sum;
-
-        if(e < (5/2)*self.m):
-           v = 0;
-           for i in range(self.m):
-               if (self.buckets[i] == 0):
+ #       print "somme="+str(sum)
+        e =  self.alpha(self.m)*float(self.m**2)*sum;
+ #       print "e="+str(e)
+        
+        v = 0;
+        for i in range(self.m):
+           if (self.buckets[i] == 0):
                    v+=1;
            if(v != 0):
                e = self.m * math.log(self.m/float(v))
 
-        if (e > 1/30 * 2**32):
-            e = -2**32 * math.log(1- e/2**32);
+ #       print v
         return e;
 
     def rho(self,val):
-        return 32 - val.bit_length() + 1;
+       rho = (32 - self.b) - val.bit_length() + 1
+       return rho
 
-
-
-#print v.bit_length()
-
-#def main():
-#    test = Hll(16);
-#    v = mmh3.hash("coucou")
-##    test.AddItem(mmh3.hash("abc"));
-#   test.AddItem(mmh3.hash("ab"));
-#    test.AddItem(mmh3.hash("abc"));
-#    print test.count();
-
-#if __name__ == '__main__':
-#  main()
